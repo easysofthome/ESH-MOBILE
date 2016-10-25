1dbded2d0930ca5ba3f5e592e6906309717b28de
XUTONGSEN <501779683@QQ.COM>
2016-10-25 17:03:05 +0800

    修改

--- src/module/msgCenter/index.js
+++ src/module/msgCenter/index.js
@@ -1,2 +1,31 @@
 require('common/js/layout.js');
 require('./index.less');
+Zepto(function($){
+$(document).ready(function (){
+   $('.news_con').each(function(){
+    var len=$(this).children(".news_text").html().length;
+    if(len<=100){
+      $(this).children(".expand").hide();
+    }else{
+      var jie=$(this).children(".news_text").html().substr();
+      var qu=$(this).children(".news_text").html().substr(0,28);
+      $(this).children(".news_text").html(qu);
+      $(this).children(".expand").click(function(){
+        if($(this).html()=="展开站内信"){
+        $(this).html("收缩站内信");
+        $(this).siblings(".news_text").html(jie);
+
+
+      }else{
+        $(this).html("展开站内信");
+        $(this).siblings(".news_text").html(qu);
+
+      }
+      })
+
+    }
+
+});
+
+})
+});
