418d5dded546eff24553dec05569df1e265e0908
XUTONGSEN <501779683@QQ.COM>
2016-11-02 09:28:41 +0800

    修改

--- src/module/easyPrice/index.js
+++ src/module/easyPrice/index.js
@@ -2,11 +2,16 @@ require('./index.less');
 Zepto(function($){
   $(".module_rg a").click(function() {
     $(this).addClass("current");
+    $(this).css('color','#128bf7');
     $(".module_lf a").removeClass("current");
+    $(".module_lf a").css('color','#383838');
   })
 
   $(".module_lf a").click(function() {
     $(this).addClass("current");
+    $(this).css('color','#128bf7');
     $(".module_rg a").removeClass("current");
+    $(".module_rg a").css('color','#383838');
+
   })
   })
